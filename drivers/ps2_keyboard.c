c9bee56e0aaaf9840347e8de25b1face58a494c9
Oscar Pavon <pavon_oscar@outlook.com>
2024-11-05 21:47:13 +0000

    a pressed and realeased

--- drivers/ps2_keyboard.c
+++ drivers/ps2_keyboard.c
@@ -1,17 +1,27 @@
 #include "ps2_keyboard.h"
+#include <stdbool.h>
+
+
+
 
 
 bool key_processed = false;
 char ps2_keyboard_get_input(byte(*port_60)()){
 	
 	byte ps2_response = port_60();
-	if(ps2_response == PS2_KEYBOARD_A_PRESSED){
+	
+
+	if(ps2_response == PS2_KEYBOARD_A_RELEASED){
 		
 			if(key_processed==false){
 				key_processed = true;
 				return 'a';
 			}
 			
+	}
+	if(ps2_response == PS2_KEYBOARD_A_PRESSED){
+		key_processed = false;
+
 	}
 
 
--- drivers/ps2_keyboard.h
+++ drivers/ps2_keyboard.h
@@ -3,7 +3,8 @@
 
 #include "../types.h"
 
-#define PS2_KEYBOARD_A_PRESSED 0x9E
+#define PS2_KEYBOARD_A_RELEASED 0x9E
+#define PS2_KEYBOARD_A_PRESSED 0x1E
 
 char ps2_keyboard_get_input(byte(*port_60)());
 
